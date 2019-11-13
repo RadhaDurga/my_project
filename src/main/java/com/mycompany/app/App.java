d7ea9d6bb426daa7161b6d6d673f6108307b1995
RadhaDurga <49023950+RadhaDurga@users.noreply.github.com>
2019-11-13 11:33:38 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello This is the deployment";
+    private final String message = "Hello This is the deployment - Version 1";
 
     public App() {}
 
